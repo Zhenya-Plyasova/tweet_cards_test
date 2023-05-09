73be5039c01f8faa1c36275768a16a177565d3e4
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-05-09 23:27:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 import { Route, Routes } from 'react-router-dom';
 
-import "./App.css";
+// import "./App.css";
 import { Layout } from "components/Layout/Layout";
 
 const HomePage = lazy(() => import('./pages/Home'));
